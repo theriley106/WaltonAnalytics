569056e97415dd7d60da4d7a88ce110f5f267ec3
Theriley106 <christopherlambert106@gmail.com>
2017-11-18 11:50:07 -0500

    Successfully added electode API to inventoryAnalytics

--- inventoryAnalytics.py
+++ inventoryAnalytics.py
@@ -11,22 +11,32 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 ELECTRODE_API = "https://www.walmart.com/product/electrode/api/state/content/{0}"
 
 
+def saveToJson(jsonDict, fileName):
+	with open('{}.json'.format(fileName.replace('.json', '')), 'w') as outfile:
+		json.dump(jsonDict, outfile) 
+
 class walShark(object):
-	def __init__(self, proxyList=[], randomizeHeaders=True, randomizeProxies=True, primaryStore=None, confirmProxy=False):
+	def __init__(self, proxyList=[], saveResponse=False, randomizeHeaders=True, randomizeProxies=True, primaryStore=None, confirmProxy=False):
 		self.proxyList = proxyList
 		self.randomizeHeaders = randomizeHeaders
 		self.randomizeProxies = randomizeProxies
 		self.primaryStore = primaryStore
+		self.saveResponse = saveResponse
 
 
 	def returnElectrode(self, SKU):
 		headers = {'accept-language': 'en-US', 'x-forwarded-for': '172.0.01', 
 				'accept': 'text/html', 'x-crawlera-cookies': 'disable', 'user-agent' : str(RandomHeaders.returnUA())}
-		url = ELECTRODE_API.format(SKU)       
-		return requests.get(url, headers=headers, proxies=random.choice(self.proxyList),verify=False, timeout=300, allow_redirects=True).json()
+		url = ELECTRODE_API.format(SKU)
+		electrode_response = requests.get(url, headers=headers, proxies=random.choice(self.proxyList),verify=False, timeout=300, allow_redirects=True)
+		if self.saveResponse == True:
+			saveToJson(electrode_response.json(), SKU)
+		return electrode_response
 
 
 r = requests.post("http://138.197.123.15:8888/proxies/{}".format(open('./SecretCode.txt').read().strip())).json()
 
-a = walShark(proxyList = r["proxies"])
-print a.returnElectrode('46118613')
+a = walShark(proxyList = r["proxies"], saveResponse=True)
+electrode_json = a.returnElectrode('46118613').json()
+if electrode_json['product'].get('priceRanges'):
+	price_range = electrode_json['product']['priceRanges'].values()[0]
