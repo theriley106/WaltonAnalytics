67068a302c17ab9ea76c73b986175187edbf7a30
Theriley106 <christopherlambert106@gmail.com>
2017-12-08 22:23:53 -0500

    changed api endpoint

--- itemGrab.py
+++ itemGrab.py
@@ -6,6 +6,7 @@ import json
 import csv
 import sys
 import time
+import subprocess
 lock = threading.Lock()
 STARTTIME = time.time()
 TIMEOUT = 10
@@ -59,6 +60,7 @@ def SearchStore(store, SKU):
 	url = "https://www.walmart.com/store/ajax/search"
 	res = requests.post(url, data=data, proxies=random.choice(Proxies))
 	res = res.json()
+	print res
 	try:
 		a["Price"] = int((GrabElement(str(res), 'priceInCents')))
 	except: 
@@ -67,6 +69,41 @@ def SearchStore(store, SKU):
 	a["Quantity"] = (GrabElement(str(res), 'quantity'))
 	return a
 
+def grabTerraFirma(store, SKU):
+	headers = {
+	    'pragma': 'no-cache',
+	    'content-type': 'application/json',
+	    'accept': '*/*',
+	    'cache-control': 'no-cache',
+	    'authority': 'www.walmart.com',
+	    'referer': 'https://www.walmart.com/product/{}/sellers'.format(SKU),
+	}
+
+	params = (
+	    ('rgs', 'OFFER_PRODUCT,OFFER_INVENTORY,OFFER_PRICE,VARIANT_SUMMARY'),
+	)
+
+	data = '{{"itemId":"{}","paginationContext":{{"selected":false}},"storeFrontIds":[{{"usStoreId":{},"preferred":false,"semStore":false}}]}}'.format(SKU, store)
+
+	response = requests.post('https://www.walmart.com/terra-firma/fetch', headers=headers, params=params, data=data, timeout=10)
+	return returnPricing(response.json())
+
+
+
+def returnPricing(terrafirmaDoc):
+	for key, value in terrafirmaDoc['payload']['offers'].items():
+		try:
+			price = terrafirmaDoc['payload']['offers'][key]['pricesInfo']['priceMap']['CURRENT']['price']
+			store = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions'][0]['storeId']
+			quantity = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions'][0]['availableQuantity']
+			return {"Store": store, "Price": price, "Quantity":quantity}
+
+			#a["Store"] = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions']['storeId']
+			#a["Quantity"] = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions']['availableQuantity']
+			#a["Price"] = terrafirmaDoc['payload']['offers'][key]['pricesInfo']['priceMap']['CURRENT']['price']
+		except Exception as exp:
+			#print exp
+			pass
 
 def chunks(l, n):
 	for i in xrange(0, len(l), n):
@@ -77,12 +114,13 @@ def searchSKU(storeList, sku):
 		if (time.time() - STARTTIME) < (60*TIMEOUT):
 			try:
 				suggestions_list = random.choice(listOfStores)
-				newData = SearchStore(store, sku)
-				newData["Price"]
-				lock.acquire()
-				print {"Store": store, "Price": newData["Price"], "Quantity": newData["Quantity"]}
-				PRIMARYDICT.append({"SKU": sku, "Store": store, "Price": int(newData["Price"]), "Quantity": newData["Quantity"]})
-				lock.release()
+				newData = grabTerraFirma(store, sku)
+				if newData != None:
+					newData["Price"]
+					lock.acquire()
+					print {"Store": store, "Price": newData["Price"], "Quantity": newData["Quantity"]}
+					PRIMARYDICT.append({"SKU": sku, "Store": store, "Price": int(newData["Price"]), "Quantity": newData["Quantity"]})
+					lock.release()
 			except Exception as exp:
 				pass
 		else:
@@ -95,24 +133,28 @@ def saveToCSV(listVar):
 		wr.writerow(list1)
 		wr.writerow([""])
 		for listItem in listVar:
-			list1 = [listItem["Store"], '${:,.2f}'.format(int(listItem["Price"]) * .01), listItem["Quantity"]]
+			list1 = [listItem["Store"], '${:,.2f}'.format(int(listItem["Price"])), listItem["Quantity"]]
 			wr.writerow(list1)
 
-listOfStores = GrabAllStoreNumbers()
+if __name__ == '__main__':
+	#val = grabTerraFirma('5751', '54594253')
+	#returnPricing(val)
 
-SKU = raw_input("Input SKU: ")
+	listOfStores = GrabAllStoreNumbers()
 
-partListOfStores = chunks(listOfStores, int(len(listOfStores)/THREADS))
-threads = [threading.Thread(target=searchSKU, args=(stores, SKU)) for stores in partListOfStores]
-for thread in threads:
-	thread.start()
-for thread in threads:
-	thread.join()
-if len(PRIMARYDICT) > 3:
-	PRIMARYDICT = sorted(PRIMARYDICT, key=lambda k: k['Price']) 
-	print("\n\n\nLowest:\nSTORE: {}\nPRICE: {}\nQUANTITY: {}\n\n\n".format(PRIMARYDICT[0]["Store"], '${:,.2f}'.format(int(PRIMARYDICT[0]["Price"]) * .01), abs(int(PRIMARYDICT[0]["Quantity"]))))
-	saveToCSV(PRIMARYDICT)
-	print("Saved Inventory Information to {}.csv\n".format(PRIMARYDICT[0]['SKU']))	
-	print("Scan Completed in {} Seconds".format(time.time() - STARTTIME))
-else:
-	print("Item Stock Too Low")
+	SKU = raw_input("Input SKU: ")
+
+	partListOfStores = chunks(listOfStores, int(len(listOfStores)/THREADS))
+	threads = [threading.Thread(target=searchSKU, args=(stores, SKU)) for stores in partListOfStores]
+	for thread in threads:
+		thread.start()
+	for thread in threads:
+		thread.join()
+	if len(PRIMARYDICT) > 3:
+		PRIMARYDICT = sorted(PRIMARYDICT, key=lambda k: k['Price']) 
+		print("\n\n\nLowest:\nSTORE: {}\nPRICE: {}\nQUANTITY: {}\n\n\n".format(PRIMARYDICT[0]["Store"], '${:,.2f}'.format(int(PRIMARYDICT[0]["Price"])), abs(int(PRIMARYDICT[0]["Quantity"]))))
+		saveToCSV(PRIMARYDICT)
+		print("Saved Inventory Information to {}.csv\n".format(PRIMARYDICT[0]['SKU']))	
+		print("Scan Completed in {} Seconds".format(time.time() - STARTTIME))
+	else:
+		print("Item Stock Too Low")
